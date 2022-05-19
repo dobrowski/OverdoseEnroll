2385821fec5356a0545acf2f0b8b33ece2ff5a0c
David Dobrowski <dobrowski@gmail.com>
2022-05-19 14:36:15 -0700

    added home language - ever EL status.

--- OverdoseEnroll.R
+++ OverdoseEnroll.R
@@ -34,3 +34,41 @@ write.csv(enrollment.od, "Enrollment in 2021-22 by select grade.csv")
 
 
 sum(enrollment.od$gr5)
+
+
+
+ELAS <- tbl(con, "ELAS")  %>%
+    filter(#YEAR == "19", # To match the last TK data file year 19-20
+        CountyName == "Monterey",
+        YEAR == "2021",
+        Grade %in% c("05", "06", "08", "09"),
+        AggLevel == "S",
+        Gender == "ALL") %>%
+    # head() %>%
+    collect() 
+
+
+elas.numb <- ELAS %>%
+    # group_by(DistrictName, SchoolName, Grade) %>%
+    # summarise(eo = sum(EO),
+    #           ifep = sum(IFEP),
+    #           el = sum(EL),
+    #           rfep = sum(RFEP),
+    #           total = sum(TotalEnrollment)
+    # ) %>%
+    transmute(DISTRICT = DistrictName,
+              SCHOOL = SchoolName,
+              Grade,
+              ever.el = IFEP+EL+RFEP) %>%
+    arrange(Grade) %>%
+    pivot_wider(names_from = Grade,
+                names_prefix = "Ever.EL.",
+                id_cols = c(DISTRICT, SCHOOL),
+                values_from = ever.el)
+
+
+od.ever.el <- enrollment.od %>% full_join(elas.numb)
+
+
+
+write.csv(od.ever.el, "Enrollment in 2021-22 by select grade with Ever EL.csv")
